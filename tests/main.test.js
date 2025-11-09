d02d23ee915293ce5c39199b90da5549992744fa
zhoushaokun <1655040956@qq.com>
2025-11-09 17:30:11 +0800

    feat: suport ue 5.7

--- tests/main.test.js
+++ tests/main.test.js
@@ -36,6 +36,18 @@ describe("main", function() {
                     expect(uasset.gatherableTextData).toStrictEqual(bytesOutput.gatherableTextData);
                 });
             });
+            describe("UE 5.7", function() {
+                /* eslint-disable-next-line no-unused-vars */
+                it.each(datacases.simple.ue57)(`$name`, ({name, input, expected}) => {
+                    const bytesInput = new Uint8Array(fs.readFileSync(path.resolve(__dirname, input)));
+                    const bytesOutput = JSON.parse(fs.readFileSync(path.resolve(__dirname, expected)).toString(), parseBigIntPayload);
+
+                    const uasset = new window.blueprintUE.uasset.ReaderUasset().analyze(bytesInput, false);
+                    expect(uasset.header).toStrictEqual(bytesOutput.header);
+                    expect(uasset.names).toStrictEqual(bytesOutput.names);
+                    expect(uasset.gatherableTextData).toStrictEqual(bytesOutput.gatherableTextData);
+                });
+            });
         });
     });
 });
